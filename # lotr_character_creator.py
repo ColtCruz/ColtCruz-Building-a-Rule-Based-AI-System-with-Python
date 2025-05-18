97a18e4ceaf42e5b2e0280d08d9eec0cd3b3ca39
Softt2 <CBCruz@student.fullsail.edu>
2025-05-18 09:53:13 -0400

    updating QOL

--- # lotr_character_creator.py
+++ # lotr_character_creator.py
@@ -1,34 +1,53 @@
-# lotr_character_creator.py 
+# lotr_character_creator.py
 
 def character_creator():
-    print("Welcome to the LOTR-Inspired MMO Character Creator!")
-    print("Available Races: Orc, Elf, Man, Dwarf, Hobbit, Goblin, Uruk")
-    print("Available Classes: Warrior, Paladin, Warlock, Lorebook, Assassin, Rogue\n")
-
-    race = input("Choose your race: ").strip().title()
-    char_class = input("Choose your class: ").strip().title()
-
     good_races = ["Man", "Elf", "Dwarf", "Hobbit"]
+    evil_races = ["Orc", "Uruk", "Goblin"]
+    all_races = good_races + evil_races
 
-    if race == "Orc" and char_class == "Paladin":
-        print("Orcs can't be Paladins. Try Warlock or Warrior.")
-    elif race in good_races and char_class == "Warlock":
-        print(f"{race}s can’t be Warlocks. Try Lorebook or Paladin.")
-    elif race == "Dwarf" and char_class == "Assassin":
-        print("Dwarves can’t be Assassins. Try Warrior or Paladin.")
-    elif race == "Hobbit" and char_class == "Warrior":
-        print("Hobbits can’t be Warriors. Try Lorebook or Rogue.")
-    elif race == "Goblin" and char_class == "Warrior":
-        print("Goblins can’t be Warriors. Try Assassin or Warlock.")
-    elif race == "Uruk" and char_class == "Assassin":
-        print("Uruks can’t be Assassins. Try Warrior or Warlock.")
-    elif race == "Orc" and char_class != "Paladin":
-        print(f"Character created: {race} {char_class}")
-    elif race in ["Man", "Elf"]:
-        print(f"Character created: {race} {char_class}")
-    else:
-        print(f"Character created: {race} {char_class}")
+    available_classes = ["Warrior", "Paladin", "Warlock", "Lorebook", "Assassin", "Ranger", "Bard"]
+
+    def print_options():
+        print("Good Races:", ", ".join(good_races))
+        print("Evil Races:", ", ".join(evil_races))
+        print("Available Classes:", ", ".join(available_classes))
+        print()
+
+    print("Welcome to the LOTR-Inspired MMO Character Creator!")
+    print_options()
+
+    while True:
+        race = input("Choose your race: ").strip().title()
+        char_class = input("Choose your class: ").strip().title()
+
+        # Validation Rules
+        if race in evil_races and char_class == "Paladin":
+            print(f"{race}s can’t be Paladins. Try Warrior or Warlock.\n")
+            print_options()
+        elif race in good_races and char_class == "Warlock":
+            print(f"{race}s can’t be Warlocks. Try Lorebook or Paladin.\n")
+            print_options()
+        elif race == "Dwarf" and char_class == "Assassin":
+            print("Dwarves can’t be Assassins. Try Warrior or Paladin.\n")
+            print_options()
+        elif race == "Hobbit" and char_class == "Warrior":
+            print("Hobbits can’t be Warriors. Try Lorebook or Assassin.\n")
+            print_options()
+        elif race == "Goblin" and char_class == "Warrior":
+            print("Goblins can’t be Warriors. Try Assassin or Warlock.\n")
+            print_options()
+        elif race == "Uruk" and char_class == "Assassin":
+            print("Uruks can’t be Assassins. Try Warrior or Warlock.\n")
+            print_options()
+        elif race not in all_races:
+            print("That race does not exist. Please choose from the list below.\n")
+            print_options()
+        elif char_class not in available_classes:
+            print("That class does not exist. Please choose from the list below.\n")
+            print_options()
+        else:
+            print(f"Character created: {race} {char_class}")
+            break
 
 if __name__ == "__main__":
     character_creator()
-
